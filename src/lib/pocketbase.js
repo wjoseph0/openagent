4e7391d431a1675870b619dcea2be7c9d61b6d4a
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2024-04-08 11:11:30 -0500

    Connect pocketbase to frontend

---
+++ src/lib/pocketbase.js
@@ -0,0 +1,10 @@
+import Pocketbase from 'pocketbase';
+import { writable } from 'svelte/store';
+
+export const pb = new Pocketbase('http://127.0.0.1:8090');
+
+export const currentUser = writable(pb.authStore.model);
+
+pb.authStore.onChange(() => {
+    currentUser.set(pb.authStore.model);
+});
